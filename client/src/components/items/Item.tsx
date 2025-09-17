4e93025ef72f638997c22e2e359b2d4f033f281c
ay-ub <ayoubhadjyoucef1@gmail.com>
2025-09-17 15:38:38 +0100

    feat: create delete item function

--- client/src/components/items/Item.tsx
+++ client/src/components/items/Item.tsx
@@ -10,10 +10,18 @@ import {
 } from "@/components/ui/card";
 import { Button } from "../ui/button";
 import { Edit, Trash } from "lucide-react";
+import PopUp from "../PopUp";
+import { DialogClose } from "../ui/dialog";
 function Item({
   item,
   ...props
 }: { item: ItemType } & React.HTMLAttributes<HTMLDivElement>) {
+  const handleRemoveItem = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    console.log("Remove item", item.id);
+    document.getElementById("cancleDeleteBtn")?.click();
+  };
+
   return (
     <Card {...props}>
       <CardHeader>
@@ -21,9 +29,26 @@ function Item({
         <CardDescription>{item.description}</CardDescription>
         <CardAction>
           <div className="flex gap-2">
-            <Button variant="outline" size="icon">
-              <Trash color="red" />
-            </Button>
+            <PopUp
+              trigger={
+                <Button variant="outline" size="icon">
+                  <Trash color="red" />
+                </Button>
+              }
+            >
+              <form>
+                <h3>Are you sure you want to delete {item.name}?</h3>
+                <p>This action cannot be undone.</p>
+                <div className="mt-4 flex justify-end gap-2">
+                  <DialogClose asChild>
+                    <Button id="cancleDeleteBtn">Cancel</Button>
+                  </DialogClose>
+                  <Button variant="destructive" onClick={handleRemoveItem}>
+                    Delete
+                  </Button>
+                </div>
+              </form>
+            </PopUp>
             <Button variant="outline" size="icon">
               <Edit />
             </Button>
