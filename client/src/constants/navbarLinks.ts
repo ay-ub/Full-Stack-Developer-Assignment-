b8d815f419207845dbc8f17be665eabc23e096d0
ay-ub <ayoubhadjyoucef1@gmail.com>
2025-09-17 20:07:34 +0100

    chore: update navLinks

--- client/src/constants/navbarLinks.ts
+++ client/src/constants/navbarLinks.ts
@@ -1,4 +1,19 @@
-import { Package, List, PlusCircle, Video, Upload } from "lucide-react";
+import {
+  Package,
+  List,
+  PlusCircle,
+  Video,
+  Upload,
+  Sliders,
+  UserCircle,
+  Settings,
+  Activity,
+  DollarSign,
+  BarChart2,
+  UserPlus,
+  Users,
+  User,
+} from "lucide-react";
 const NAV_MAIN = [
   {
     id: "items-section",
@@ -31,6 +46,66 @@ const NAV_MAIN = [
         title: "Upload Video",
         url: "#video-form",
         icon: Upload,
+      },
+    ],
+  },
+  {
+    id: "users-section",
+    title: "Users",
+    url: "#users",
+    icon: User,
+    items: [
+      {
+        id: "user-list",
+        title: "All Users",
+        url: "#user-list",
+        icon: Users,
+      },
+      {
+        id: "user-add",
+        title: "Add User",
+        url: "#user-add",
+        icon: UserPlus,
+      },
+    ],
+  },
+  {
+    id: "reports-section",
+    title: "Reports",
+    url: "#reports",
+    icon: BarChart2,
+    items: [
+      {
+        id: "sales-report",
+        title: "Sales",
+        url: "#sales-report",
+        icon: DollarSign,
+      },
+      {
+        id: "activity-report",
+        title: "Activity",
+        url: "#activity-report",
+        icon: Activity,
+      },
+    ],
+  },
+  {
+    id: "settings-section",
+    title: "Settings",
+    url: "#settings",
+    icon: Settings,
+    items: [
+      {
+        id: "profile-settings",
+        title: "Profile",
+        url: "#profile-settings",
+        icon: UserCircle,
+      },
+      {
+        id: "app-settings",
+        title: "Application",
+        url: "#app-settings",
+        icon: Sliders,
       },
     ],
   },
